826aa32e2dac794ef3db584b76a64cc46aff8d98
devil <srinath1@users.noreply.github.com>
2017-11-25 19:59:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-var server = require('./toneExample.js');
+var http = require('http');
+var bodyParser = require("body-parser");
+var express = require('express');
+var router = express();
+
+router.use(bodyParser.urlencoded({extended: false}));
+router.use(bodyParser.json());
+
+router.post('/info', function(req, res) {
+    var text = req.body.text;
+    console.log(text)
+    var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+
+    var tone_analyzer = new ToneAnalyzerV3({
+     username: 'yourUsername',
+     password: 'yourPassword',
+     version_date: '2016-05-19'
+     });
+
+    tone_analyzer.tone({
+        text: text
+    }, function(err, tone) {
+    if (err) console.log(err);
+    else res.send((JSON.stringify(tone, null, 2)));
+    })
+})
+
+
+
 var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
 
-server.listen(port, function() {
+router.listen(port, function() {
   console.log('Server running on port: %d', port);
 });
